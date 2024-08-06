1edbedeee122391fc33f26e87bb7b8a4f12d5402
wrefinity <wrefordmessi@gmail.com>
2024-08-06 16:47:19 +0100

    form designing

--- index.php
+++ index.php
@@ -7,25 +7,27 @@
 </head>
 
 <body>
-    
-    <div class="d-flex m-4 justify-content-center align-items-center">
-        <div class="auth-form">
-        <h4 class="text-center mb-4">Login</h4>
-        <form id="loginForm" action="POST">
-            <div class="mb-3">
-                <label for="email" class="form-label">Email:</label>
-                <input type="email" class="form-control" id="email" name="email" required>
+
+    <div class="container">
+        <div class=" d-flex m-4 justify-content-center align-items-center">
+            <div class="auth-form">
+                <h4 class="text-center mb-4">TARABA STATE PAYROLL SYSTEM</h4>
+                <h4 class="text-center mb-4"><i> Login </i></h4>
+                <form id="loginForm" action="POST">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email:</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password:</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="d-grid">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password:</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <div class="d-grid">
-                <button type="submit" class="btn btn-primary">Login</button>
-            </div>
-        </form>
         </div>
     </div>
 </body>
-
 </html>
